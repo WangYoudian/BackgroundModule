8b8d6103a00812895f4bb76bb58bf8624c0e3c28
Bruce Wang <486590722@qq.com>
2020-03-27 00:03:08 +0800

    restructure of mqtt client

---
+++ MQTT-Client/config.py
@@ -0,0 +1,1 @@
+OAUTH_CHECK_URL = "http://api.std.corepro.10.124.130.142.xip.io/corepro/auth/mqtt"
---
+++ MQTT-Client/mqtt_client.py
@@ -0,0 +1,11 @@
+from
+
+class MqttClient:
+    # dependency injection
+    def __init__(self):
+        pass
+    
+    def __on_connect(self):
+        pass
+
+client = MqttClient()
